d9502a9d03b7645daac82b4c2c6b39f7eabce813
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-13 21:14:47 +0100

    FlatMapLaziness.java

---
+++ src/test/java/FlatMapLaziness.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by mtumilowicz on 2018-11-13.
+ */
+public class FlatMapLaziness {
+    @Test
+    public void flatMap_laziness_unlimitedStream() {
+       Stream.of(1)
+                .flatMap(x -> Stream.generate(Math::random))
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+    }
+
+    @Test
+    public void flatMap_laziness_limitedStream() {
+        Integer d = Stream.of(1)
+                .flatMap(x -> Stream.iterate(0, i -> ++i).peek(System.out::println).limit(10))
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+
+        assertThat(d, is(0));
+    }
+}
